d7948680b5893c0afa6ab30c23ae4b03efb72f0b
ahmadalpadani <ahmadalpadani26@gmail.com>
2024-02-18 16:17:28 +0700

    modified:   problem1/main.py #tugas 1 modified:   problem2/main.py #tugas 2 modified:   problem3/main.py #tugas 3 modified:   problem4/main.py #tugas 4 new file:   running_test/Problem-1.png #ss new file:   running_test/Problem-2.png #ss new file:   running_test/Problem-3.png #ss new file:   running_test/Problem-4.png #ss

--- problem1/main.py
+++ problem1/main.py
@@ -3,3 +3,6 @@ nama = "Kobar"
 
 
 # kode disini
+kalimat = "Hello " + nama + "!"
+kalimat_akhir = kalimat + " Saya Golang bahasa yang sangat menyenangkan"
+print (kalimat_akhir)
--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,13 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+
+#input
+Harga = 370000
+Diskon = 15
+
+#kode disini
+Harga_diskon = (Diskon/100)*Harga
+Harga_akhir = Harga - Harga_diskon
+
+print(Harga_akhir)
